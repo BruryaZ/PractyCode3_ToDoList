936f03a553f9e2c77593951f0afb4883dc4a0d9b
Brurya-Zarbiv <bz0556758244@gmail.com>
2025-02-04 20:08:59 +0200

    Corect name Client

--- TodoApi/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TodoApi.Models
-{
-    public class LoginModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- TodoApi/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApi.Models
-{
-    public class User
-    {
-        public int ID { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
